6b894f9f3ab648b97bf614d90fd5ef5b3f8fc636
vitaliyGoncharenko <goncharenko.gvv@gmail.com>
2015-10-19 20:20:29 +0300

    refactoring dao and BonusesDao

--- DAL/src/main/java/taxiService/service/db/impl/BonusesDao.java
+++ DAL/src/main/java/taxiService/service/db/impl/BonusesDao.java
@@ -2,6 +2,7 @@ package taxiService.service.db.impl;
 
 import org.javalite.activejdbc.Model;
 import taxiService.model.Bonuses;
+import taxiService.model.Customer;
 import taxiService.service.db.ITemplateDao;
 import taxiService.transfer.BaseDto;
 import taxiService.transfer.BonusesDto;
@@ -11,37 +12,37 @@ import java.util.List;
 /**
  * Created by Vitaliy on 17.10.2015.
  */
-public class BonusesDao implements ITemplateDao {
+public class BonusesDao implements ITemplateDao<BonusesDto> {
     public Model get(String templateName) {
         return Bonuses.findFirst("countBonuses", templateName);
     }
 
     public Model get(Long id) {
-        return Bonuses.findFirst("id", id);;
+        return Bonuses.findFirst("id", id);
     }
 
+    @Override
     public Model create(BonusesDto bonusesDto) {
         Bonuses bonuses = new Bonuses();
         bonuses.setCountBonuses(bonusesDto.getCountBonuses());
         bonuses.setDiscount(bonusesDto.getDiscount());
 
-        if (bonusesDto.getTemplateId() != null)
-            field.setParent(Template.findById(fieldDto.getTemplateId()));
-
-        if (fieldDto.getTypeFieldName() != null)
-            field.setParent(TypeField.findFirst("name = ?", fieldDto.getTypeFieldName()));
-
-        field.saveIt();
-        return field;
+        if (bonuses.getCustomerId() != null) {
+            bonuses.setParent(Customer.findById(bonusesDto.getCustomerId()));
+        }
+        bonuses.saveIt();
+        return bonuses;
     }
 
     public void delete(String templateName) {
 
     }
 
-    public void update(BaseDto dto) {
+    @Override
+    public void update(BonusesDto dto) {
 
     }
+
 
     public void delete(Long id) {
 
--- Model/src/main/java/taxiService/model/Bonuses.java
+++ Model/src/main/java/taxiService/model/Bonuses.java
@@ -1,6 +1,7 @@
 package taxiService.model;
 
 import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.BelongsTo;
 import org.javalite.activejdbc.annotations.Table;
 import taxiService.transfer.BonusesDto;
 
@@ -8,6 +9,7 @@ import taxiService.transfer.BonusesDto;
  * Created by Vitaliy on 14.10.2015.
  */
 @Table("bonuses")
+@BelongsTo(parent = Customer.class, foreignKeyName = "customer_id")
 public class Bonuses extends Model {
 
     public Long getId() {
@@ -32,6 +34,14 @@ public class Bonuses extends Model {
 
     public void setDiscount(Long discount) {
         setLong("discount", discount);
+    }
+
+    public Long getCustomerId() {
+        return getLong("customer_id");
+    }
+
+    public void setCustomerId(Long customerId) {
+        setLong("customer_id", customerId);
     }
 
     public BonusesDto toDto(){
